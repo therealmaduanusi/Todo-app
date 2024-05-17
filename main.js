06dc44a359396aef9e185453879b04e2223eaf13
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-05-17 14:07:38 +0100

    update

---
+++ main.js
@@ -0,0 +1,46 @@
+function addTask() {
+  var taskInput = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+
+  if (taskInput.value === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  var li = document.createElement("li");
+  var taskText = document.createElement("span");
+  taskText.textContent = taskInput.value;
+
+  var editInput = document.createElement("input");
+  editInput.type = "text";
+  editInput.style.display = "none";
+
+  var editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.classList.add("edit-button");
+  editButton.onclick = function() {
+    if (editInput.style.display === "none") {
+      taskText.style.display = "none";
+      editInput.value = taskText.textContent;
+      editInput.style.display = "inline-block";
+    } else {
+      taskText.textContent = editInput.value;
+      editInput.style.display = "none";
+      taskText.style.display = "inline-block";
+    }
+  };
+  
+  var deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'delete';
+  deleteBtn.onclick = function () {
+    taskList.removeChild(li)
+  }
+
+  li.appendChild(taskText);
+  li.appendChild(editInput);
+  li.appendChild(editButton);
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+  
